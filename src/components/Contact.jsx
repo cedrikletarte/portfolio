3ed5dab8b580624246f0f02feb265df804009e88
cedrik <cedrikletarte@gmail.com>
2025-06-21 15:56:25 -0400

    fix : contact layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,11 @@ const Contact = () => {
 
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center'>
-         <form method='POST' action='https://getform.io/f/933db683-e35f-4f49-964f-d25ca92b8846' className='flex flex-col max-w-[600px] w-full'>
+         <form
+           method='POST'
+           action='https://getform.io/f/933db683-e35f-4f49-964f-d25ca92b8846'
+           className='flex flex-col max-w-[600px] w-full px-4' // Ajoute px-4 ici
+         >
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>{t('contact.title')}</p>
                 <p className='text-gray-300 py-4'>
@@ -27,9 +31,9 @@ const Contact = () => {
                   })}
                 </p>
             </div>
-            <input className='bg-[#ccd6f6] p-2' type='text' placeholder={t('contact.name')} name='name' required/>
-            <input className='my-4 p-2 bg-[#ccd6f6]' type='text' pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title={t('contact.emailTitle')} placeholder={t('contact.email')} name='email' required/>
-            <textarea className='bg-[#ccd6f6] p-2' name='message' rows={6} placeholder={t('contact.message')} required />
+            <input className='bg-[#ccd6f6] text-black p-2' type='text' placeholder={t('contact.name')} name='name' required/>
+            <input className='my-4 text-black p-2 bg-[#ccd6f6]' type='text' pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title={t('contact.emailTitle')} placeholder={t('contact.email')} name='email' required/>
+            <textarea className='bg-[#ccd6f6] text-black p-2' name='message' rows={6} placeholder={t('contact.message')} required />
             <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>
               {t('contact.button')}&nbsp;<MdSend/>
             </button>
